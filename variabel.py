7500b74872ccf40754c6cd5c1b4ff366994b9357
zeyaimut <yogafirmasa@gmail.com>
2025-05-06 08:21:30 +0700

    variabel.py

---
+++ variabel.py
@@ -0,0 +1,21 @@
+nama = "rosela"
+umur = "16"
+
+print(nama)
+print(umur)
+print(type(umur))
+
+umur = "enam belas"
+print(umur)
+print(type(umur))
+
+namaDepan = "rosela"
+namaBelakang = "ajara"
+namaLengkap = namaDepan + " " + namaBelakang
+hobi = "menuls diary"
+print("biodata: ", namaLengkap, umur, hobi)
+
+panjang = 7
+lebar = 3
+luas = panjang*lebar
+print("luas: ", luas)
