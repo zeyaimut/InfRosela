9fb6fbd25cd4ffceb958a0f24959e932acb4051a
zeyaimut <yogafirmasa@gmail.com>
2025-05-06 08:19:55 +0700

    tipedata.py

---
+++ tipedata.py
@@ -0,0 +1,36 @@
+# Tipe data Boolean
+print(False)
+
+# Tipe data String
+print("Python itu seru")
+print('Belajar ngoding bikin nagih')
+
+# Tipe data Integer
+print(100)
+
+# Tipe data Float
+print(2.718)
+
+# Tipe data Hexadecimal
+print("Hexadecimal dari 45 adalah " + hex(45))
+
+print("hobi saya : " + "membaca")
+
+# Tipe data Complex
+print(4+2j)
+
+# Tipe data List
+print([10, 20, 30])
+print(["apel", "jeruk", "mangga"])
+
+# Tipe data Tuple
+print((7, 8, 9))
+print(("merah", "kuning", "hijau"))
+
+# Tipe data Dictionary
+print({"kota": "Jakarta", "kode_pos": 10110})
+
+# Tipe data Dictionary dimasukkan ke dalam variabel info
+info = {"kota": "Bandung", "kode_pos": 40286}  # inisialisasi variabel info
+print(info)  # menampilkan isi dictionary
+print(type(info))  # mengecek tipe data variabel
