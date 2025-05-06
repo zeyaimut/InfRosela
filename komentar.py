4e35eeb67c1c8be30678285c96a595472ec42592
zeyaimut <yogafirmasa@gmail.com>
2025-05-06 08:16:58 +0700

    komentar.py

---
+++ komentar.py
@@ -0,0 +1,8 @@
+# progeam menghitung luas panjang degan input dari user
+#meminta input panjang dan lebar dari pengguna
+panjang = float(input("masukan panjang(meter): "))
+lebar = float(input("masukan lebar(meter): "))
+#menghitung luas
+luas = panjang * lebar
+#menampilkan hasl
+print("luas persegi panjang adalah:", luas, "meter persegi")
