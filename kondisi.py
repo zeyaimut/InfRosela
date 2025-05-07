a46e4e5f305b404bc44fbbcd9e406074e77c6e09
zeyaimut <yogafirmasa@gmail.com>
2025-05-07 20:47:02 +0700

    kondisi

---
+++ kondisi.py
@@ -0,0 +1,10 @@
+nilai = int(input("Masukkan nilai ujian: "))
+
+if nilai >= 90:
+    print("Nilai: A")
+elif nilai >= 80:
+    print("Nilai: B")
+elif nilai >= 70:
+    print("Nilai: C")
+else:
+    print("Nilai: D")
